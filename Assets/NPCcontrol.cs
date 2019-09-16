14b13b76e575da675f4f726ae7937d263da92b34
pablo correa <juanpabloc2000@gmail.com>
2019-09-16 15:13:05 -0500

    Initial commit

---
+++ Assets/NPCcontrol.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCcontrol : MonoBehaviour
+{
+    public GameObject reference;
+    public static GameObject flyclaw;
+    void Start()
+    {
+        flyclaw = GameObject.Instantiate(reference) as GameObject;
+        flyclaw.transform.position = new Vector3(0, 4, 0);
+        flyclaw.AddComponent<Flyclaw>();
+
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
+
+public class Flyclaw : MonoBehaviour
+{
+
+}
