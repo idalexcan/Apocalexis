1e00f2c75e8105fa2468fb7c5a05dfcd776eb2ea
idalexcan <id.alexcan@gmail.com>
2019-09-27 17:42:55 -0500

    Iniciando el ataque del heroe

--- Assets/HeroControl.cs
+++ Assets/HeroControl.cs
@@ -6,6 +6,11 @@ public class HeroControl : MonoBehaviour
 {
     public static float velocity = 5;
     public static float speed;
+    
+    private void Awake()
+    {
+        GameObject.Find("Impulser").AddComponent<Proyectile>();
+    }
     private void Update()
     {
         GameObject.Find("Head").transform.eulerAngles = GameObject.Find("Main Camera").transform.eulerAngles;
@@ -45,8 +50,37 @@ public class HeroControl : MonoBehaviour
         
     }
 
+
+
 }
 
 
+public class Proyectile : MonoBehaviour
+{
+    float impulse;
+    float impulseaux;
+    bool shot;
 
+    private void Update()
+    {
+        Debug.Log(impulse);
+        if (Input.GetKey(KeyCode.Space))
+        {
+            impulse += 25;
+            
+        }
+        else if (impulse!=0)
+        {
+            shot = true;
+            impulseaux = impulse;
+            impulse = 0;
+        }
+        if (shot)
+        {
+            GetComponent<Rigidbody>().AddForce(transform.forward * impulseaux);
+            Debug.Log("LANZAMIENTO!---------->"+impulseaux);
+            shot = false;
+        }
+    }
+}
 
